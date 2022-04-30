9bb3a66049b1f956d182d0aaa9bc45e23c08073f
Kareem ElZeky <kareem.elzeky@gmail.com>
2022-04-30 14:55:22 +0200

    added the main routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const {createMeeting} = require('./server/db.js');
 module.exports = app;
 
 /* Do not change the following line! It is required for testing and allowing
@@ -21,10 +21,63 @@ app.use(bodyParser.json());
 const apiRouter = require('./server/api');
 app.use('/api', apiRouter);
 
+//id counter for minions and ideas
+let nextMinionId = 0,  nextIdeaId = 0;
+
+// Routes for api/minions/
+apiRouter.get('/minions/', (req, res, next) => {
+
+});
+
+apiRouter.post('/minions/', (req, res, next) => {
+  const newMinion = req.body;
+  
+});
+apiRouter.get('/minions/:minionId/', (req, res, next) => {
+
+});
+apiRouter.put('/minions/:minionId/', (req, res, next) => {
+
+});
+apiRouter.delete('/minions/:minionId/', (req, res, next) => {
+
+});
+// Routes for api/ideas/
+
+apiRouter.get('/ideas/', (req, res, next) => {
+
+});
+
+apiRouter.post('/ideas/', (req, res, next) => {
+
+});
+apiRouter.get('/ideas/:ideaId/', (req, res, next) => {
+
+});
+apiRouter.put('/ideas/:ideaId/', (req, res, next) => {
+
+});
+apiRouter.delete('/ideas/ideaId/', (req, res, next) => {
+
+});
+
+// Routes for api/meetings
+
+apiRouter.get('/meetings/', (req, res, next)=> {
+
+});
+apiRouter.post('/meetings/', (req, res, next) => {
+  createMeeting()
+});
+apiRouter.delete('/meetings/', (req, res, next) => {
+
+});
+
+
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-  })
+  });
 }
