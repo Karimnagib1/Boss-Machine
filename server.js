7239a84d4a3685f8afdc52e15c8c161506baeb69
Kareem ElZeky <kareem.elzeky@gmail.com>
2022-04-30 17:35:59 +0200

    added routes logic

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const {createMeeting} = require('./server/db.js');
+const {createMeeting, getAllFromDatabase, getFromDatabaseById, addToDatabase, deleteFromDatabasebyId, deleteAllFromDatabase} = require('./server/db.js');
 module.exports = app;
 
 /* Do not change the following line! It is required for testing and allowing
@@ -26,51 +26,109 @@ let nextMinionId = 0,  nextIdeaId = 0;
 
 // Routes for api/minions/
 apiRouter.get('/minions/', (req, res, next) => {
-
+  const minions = getAllFromDatabase('minions');
+  res.send(minions);
 });
 
 apiRouter.post('/minions/', (req, res, next) => {
   const newMinion = req.body;
-  
+  if (newMinion.salary && newMinion.title && newMinion.name && newMinion.weaknesses){
+    newMinion.salary = Number(newMinion.salary);
+    const newMinionWId = addToDatabase('minions', newMinion);
+    res.status(201).send(newMinionWId);
+  } else {
+    res.status(400).send();
+  }
 });
 apiRouter.get('/minions/:minionId/', (req, res, next) => {
-
+  const minion = getFromDatabaseById('minions', req.params.minionId);
+  if(minion){
+    res.send(minion);
+  } else {
+    res.status(404).send();
+  }
 });
 apiRouter.put('/minions/:minionId/', (req, res, next) => {
-
+  let updatedMinion = req.body;
+  if (updatedMinion.name && updatedMinion.title && updatedMinion.salary && updatedMinion.weaknesses){
+    updatedMinion.salary = Number(updatedminion.salary);
+    updatedMinion = updateInstanceInDatabase('minions', updatedMinion);
+    if (updatedMinion){
+      return res.status(201).send(updatedMinion);
+    } 
+  }
+  res.status(404).send();
 });
 apiRouter.delete('/minions/:minionId/', (req, res, next) => {
-
+  const id = Number(req.params.minionId);
+  const deleted = deleteFromDatabasebyId('minions', id);
+  if (deleted) {
+    res.status(200).send();
+  } else {
+    res.status(404).send();
+  }
 });
 // Routes for api/ideas/
 
 apiRouter.get('/ideas/', (req, res, next) => {
-
+  const ideas = getAllFromDatabase('ideas');
+  res.send(ideas);
 });
 
 apiRouter.post('/ideas/', (req, res, next) => {
-
+  const newIdea = req.body;
+  if (newIdea.name && newIdea.description && newIdea.weeklyRevenue && newIdea.numWeeks){
+    newIdea.numWeeks = Number(newIdea.numWeeks);
+    newIdea.weeklyRevenue = Number(newIdea.weeklyRevenue);
+    const newIdeaWId = addToDatabase('ideas', newIdea);
+    res.status(201).send(newIdeaWId);
+  } else {
+    res.status(400).send();
+  }
 });
 apiRouter.get('/ideas/:ideaId/', (req, res, next) => {
-
+  const idea = getFromDatabaseById('ideas', req.params.ideaId);
+  if(idea){
+    res.send(idea);
+  } else {
+    res.status(404).send();
+  }
 });
 apiRouter.put('/ideas/:ideaId/', (req, res, next) => {
-
+  let updatedIdea = req.body;
+  if (updatedIdea.name && updatedIdea.description && updatedIdea.weeklyRevenue && updatedIdea.numWeeks){
+    updatedIdea.weeklyRevenue = Number(updatedIdea.weeklyRevenue);
+    updatedIdea.numWeeks = Number(updatedIdea.numWeeks);
+    updatedIdea = updateInstanceInDatabase('ideas', updatedIdea);
+    if (updatedIdea){
+      return res.status(201).send(updatedIdea);
+    } 
+  }
+  res.status(404).send();
 });
-apiRouter.delete('/ideas/ideaId/', (req, res, next) => {
-
+apiRouter.delete('/ideas/:ideaId/', (req, res, next) => {
+  const id = Number(req.params.ideaId);
+  const deleted = deleteFromDatabasebyId('ideas', id);
+  if (deleted) {
+    res.status(200).send();
+  } else {
+    res.status(404).send();
+  }
 });
 
 // Routes for api/meetings
 
 apiRouter.get('/meetings/', (req, res, next)=> {
-
+  const meetings = getAllFromDatabase('meetings');
+  res.send(meetings);
 });
 apiRouter.post('/meetings/', (req, res, next) => {
-  createMeeting()
+  const newMeeting = createMeeting();
+  res.send(newMeeting);
 });
 apiRouter.delete('/meetings/', (req, res, next) => {
-
+  deleteAllFromDatabase('meetings');
+  res.status(200).send();
 });
 
 
